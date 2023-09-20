d9fa77f8874e7c0d457b47f079e38d6b9e5bd92c
Amanda Alves <amandaalves51030@gmail.com>
2023-09-20 16:23:58 -0300

    Criado

---
+++ Projeto-Back/src/main/java/model/User.java
@@ -0,0 +1,22 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String userName;
+	private String name;
+	private String email;
+	
+}
---
+++ Projeto-Back/src/test/java/com/javaereact/Projeto/CadastroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaereact.Projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
